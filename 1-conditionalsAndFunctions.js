e54d271513526831480cf574c1afcb617191be4f
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-10-02 19:00:20 +0530

    L1toL6 Updated

---
+++ 1-conditionalsAndFunctions.js
@@ -0,0 +1,40 @@
+//if-else, else if ladder, switch
+
+// if(!true){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+// var a=10
+// var b=20
+// switch(a){
+//     case 10:
+//         console.log("It is 10")
+//         break;
+//     default:
+//         console.log("It was something else")
+// }
+
+//Looping Constructs (Loops)
+// for , while, nested 
+
+//functions
+
+function doSomething(){
+    console.log('Doing')
+}
+doSomething()
+
+function add(a,b){
+    var c = a+b;
+    console.log(c)
+}
+add(10,20)
+
+//storing value in array
+var train = ['wheat','berley','potato']
+console.log(train[0])
+console.log(train.length)
+
