a51d1b4050524b50ac11e33b02dfdd5788b844fa
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-10-07 23:59:04 +0530

    DS intro added

--- 3-TemplateLiteral.js
+++ 3-TemplateLiteral.js
@@ -12,3 +12,25 @@ let para = `Hi! My name is xyz
 And I am working with the Dev Team
 `
 console.log(para)
+
+
+
+// ---------------------------
+
+
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+function birdCan() {
+    for(prop of Object.entries(bird)){
+        console.log(`${prop[0]}: ${prop[1]}`)
+    }
+}
+birdCan()
+
+function animalCan(){
+    for(prop in bird){
+        console.log(`${prop}: ${bird[prop]}`)
+    }
+}
+animalCan()
