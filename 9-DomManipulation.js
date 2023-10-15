959714a41b87542fa8039bb406e7504af2bb2813
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-10-15 23:31:34 +0530

    DOM Manipulation added

---
+++ 9-DomManipulation.js
@@ -0,0 +1,18 @@
+//DOM tree structure: HTML file is mapped out like a tree structure
+// with nested objects for different parts of the page 
+
+//DOM: A browser builds a DOM, or model, for every web page 
+// it downloads as a JavaScript object
+
+//DOM Manipulation: We can use JS to manipulate the HTML DOM
+
+
+//Basic DOM manipulation
+// const h2 = document.createElement('h2')
+
+// h2.innerText = 'Some new text'
+
+// h2.setAttribute('id','h2TextID')
+
+// h2.setAttribute('class','h2TextClass')
+// document.body.append(h2)
