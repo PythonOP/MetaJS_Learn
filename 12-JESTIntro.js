46d27ead004ac7784be2c9091a823dee293ad2a2
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-10-19 23:44:29 +0530

    JestIntroduction

---
+++ 12-JESTIntro.js
@@ -0,0 +1,11 @@
+// JEST is a testing framework
+// We use expect()
+function concatStr(s1,s2){
+    return s1+s2;
+}
+console.log(concatStr("abc","def"));
+// expect(concatStr("abc","def")).toBe("abcdef")
+
+
+//Testing types:
+// e2e, Integration, Unit
