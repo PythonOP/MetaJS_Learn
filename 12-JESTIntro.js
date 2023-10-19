9a64cbd38a15e47f80b5452ee957b19eddf8a803
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-10-19 23:49:47 +0530

    JestIntroduction

--- 12-JESTIntro.js
+++ 12-JESTIntro.js
@@ -9,3 +9,6 @@ console.log(concatStr("abc","def"));
 
 //Testing types:
 // e2e, Integration, Unit
+// e2e end to end testing are slow testing methods to manual check systems
+// Integration testing tests how parts of system interacts with each other
+// Unit testing tests the smallest units of the system(self-contained codes to check subprocesses)
